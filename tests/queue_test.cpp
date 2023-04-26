c3700c22af234251eae9b017138cb984ea137aca
amyangfei <amyangfei@gmail.com>
2023-04-26 19:34:36 +0800

    fix lock unexpected hold in unit test

--- tests/queue_test.cpp
+++ tests/queue_test.cpp
@@ -11,8 +11,9 @@
 TEST(QueueTest, BoundedQueue) {
   std::unordered_map<int, int> m;
   std::mutex map_mutex;
-  bounded_queue<int> q{10};
+  bounded_queue<int> q{16};
   int N = 2000;
+  int consumer_count = 5;
   auto produce = [&]() {
     for (int i = 0; i < N; ++i) {
       q.push(i);
@@ -26,20 +27,26 @@ TEST(QueueTest, BoundedQueue) {
       m[idx] = cid;
     }
   };
-  std::thread producer1(std::bind(produce));
-  std::thread consumer1(std::bind(consume, 1));
-  std::thread consumer2(std::bind(consume, 2));
+  std::thread producer1(produce);
+  std::thread consumers[consumer_count];
+  for (int i = 0; i < consumer_count; ++i) {
+    consumers[i] = std::thread(std::bind(consume, i));
+  }
 
   producer1.join();
-  for (int i = 0; i < 20; ++i) {
-    std::lock_guard<std::mutex> guard(map_mutex);
-    if (m.size() >= N) {
-      break;
+
+  for (int i = 0; i < 40; ++i) {
+    {
+      std::lock_guard<std::mutex> guard(map_mutex);
+      if (m.size() == N) {
+        break;
+      }
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
   }
   q.done();
-  consumer1.join();
-  consumer2.join();
+  for (auto &c : consumers) {
+    c.join();
+  }
   ASSERT_EQ(m.size(), N);
 }
